cb67232643e3002a5b2cc701eb18e94267d170df
njberman <njberman16@hotmail.com>
2023-01-17 20:34:21 +0000

    Change to using socket to create new game hopefully to work on chromebooks

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const WebSocket = require('ws');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const fs = require('fs');
 
 const Game = require('./game');
 // import express from 'express';
@@ -68,7 +69,6 @@ const wss = new WebSocket.Server({ server });
 wss.on('connection', (ws) => {
   //connection is up, let's add a simple simple event
   ws.on('message', (message) => {
-    console.log(message);
     message = String(message);
     //log the received message and send it back to the client
     console.log(`Received: ${message}`);
@@ -80,8 +80,51 @@ wss.on('connection', (ws) => {
       game.move(newMessage[2]);
       wss.clients.forEach((client) => client.send(message));
     }
+
+    if (message.includes('new')) {
+      const newMessage = message.split(' ').filter((v) => v.length > 0);
+      if (newMessage.length > 1) {
+        if (findGame(newMessage[1])) {
+          ws.send(
+            JSON.stringify({
+              message: `Game already exists with code: ${
+                findGame(newMessage[1]).code
+              }`,
+              code: newMessage[1],
+            }),
+          );
+        } else {
+          games.push(new Game(newMessage[1]));
+          ws.send(
+            JSON.stringify({
+              message: `New game created with code: ${newMessage[1]}`,
+              code: newMessage[1],
+            }),
+          );
+          writeNewGame();
+        }
+      } else {
+        games.push(new Game());
+        ws.send(
+          JSON.stringify({
+            message: `New game created with code: ${
+              games[games.length - 1].code
+            }`,
+            code: games[games.length - 1].code,
+          }),
+        );
+        writeNewGame();
+      }
+    }
   });
 });
+
+const writeNewGame = () => {
+  // fs.writeFile('./games.json', JSON.stringify(games), (e) => {
+  //   if (e) throw e;
+  // });
+  console.log('writing new game');
+};
 
 //start our server
 server.listen(process.env.PORT || 8999, () => {
