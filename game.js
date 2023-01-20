2403ea495fdaf97b566fe134900a2889555f94c4
njberman <njberman16@hotmail.com>
2023-01-20 15:45:22 +0000

    Change to using fen instead of move

--- game.js
+++ game.js
@@ -1,11 +1,38 @@
 const { Chess } = require('chess.js');
 
+const COLOUR = {
+  WHITE: 'white',
+  BLACK: 'black',
+};
+
 const ABC = 'abcdefghijklmnopqrstuvwxz';
 
 class Game {
   constructor(code) {
     this.code = code || this.genCode(5);
     this.game = new Chess();
+    this.toConnect = COLOUR.WHITE;
+    this.connected = [];
+    this.taken = [];
+  }
+
+  connect(ip) {
+    if (this.connected.length < 2) {
+      this.connected.push({ colour: this.toConnect, ip });
+      this.toConnect = COLOUR.BLACK;
+    } else {
+      // for (let i = 0; i < 2; i++) {
+      //   if (this.connected[i].ip === ip) {
+      //     this.toConnect =
+      //       this.connected[i].colour === COLOUR.WHITE
+      //         ? COLOUR.BLACK
+      //         : COLOUR.WHITE;
+      //   } else {
+      this.toConnect = 'spec';
+      // }
+      // }
+    }
+    console.log(this.connected);
   }
 
   genCode(len) {
@@ -20,9 +47,8 @@ class Game {
     return code;
   }
 
-  move(alMove) {
-    console.log(alMove);
-    this.game.move(alMove);
+  move(fen) {
+    this.game = new Chess(fen);
   }
 }
 
--- index.js
+++ index.js
@@ -5,17 +5,12 @@ const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const fs = require('fs');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const Game = require('./game');
-// import express from 'express';
-// import http from 'http';
-// import WebSocket from 'ws';
-// import cors from 'cors';
-// import helmet from 'helmet';
-// import morgan from 'morgan';
-// import dotenv from 'dotenv';
 
 require('dotenv').config();
+const { URI } = process.env;
 
 const games = [];
 
@@ -26,6 +21,18 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
+const client = new MongoClient(URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+// client
+//   .connect()
+//   .then(async () => {
+const db = client.db('games');
+const collection = db.collection('games');
+// const games = async () => await collection.find({}).toArray();
 app.post('/new-game', (req, res) => {
   if (req.body && req.body.code) {
     if (findGame(req.body.code)) {
@@ -73,11 +80,22 @@ wss.on('connection', (ws) => {
     //log the received message and send it back to the client
     console.log(`Received: ${message}`);
 
+    if (message.includes('take')) {
+      const game = findGame(message.split(' ')[1]);
+      if (message.includes('taken')) {
+        ws.send(JSON.stringify({ taken: game.taken }));
+        console.log(game.taken);
+      } else {
+        game.taken.push(message.split(' ')[2]);
+        console.log(game.taken);
+      }
+    }
+
     if (message.includes('move')) {
       // Parse move data
       const newMessage = message.split(' ');
       const game = findGame(newMessage[0]);
-      game.move(newMessage[2]);
+      game.move(message.split('"')[1]);
       wss.clients.forEach((client) => client.send(message));
     }
 
@@ -85,12 +103,15 @@ wss.on('connection', (ws) => {
       const newMessage = message.split(' ').filter((v) => v.length > 0);
       if (newMessage.length > 1) {
         if (findGame(newMessage[1])) {
+          findGame(newMessage[1]).connect(ws._socket.remoteAddress);
           ws.send(
             JSON.stringify({
               message: `Game already exists with code: ${
                 findGame(newMessage[1]).code
               }`,
               code: newMessage[1],
+              fen: findGame(newMessage[1]).game.fen(),
+              toConnect: findGame(newMessage[1]).toConnect,
             }),
           );
         } else {
@@ -99,9 +120,11 @@ wss.on('connection', (ws) => {
             JSON.stringify({
               message: `New game created with code: ${newMessage[1]}`,
               code: newMessage[1],
+              fen: games[games.length - 1].game.fen(),
             }),
           );
           writeNewGame();
+          games[games.length - 1].connect(ws._socket.remoteAddress);
         }
       } else {
         games.push(new Game());
@@ -111,9 +134,11 @@ wss.on('connection', (ws) => {
               games[games.length - 1].code
             }`,
             code: games[games.length - 1].code,
+            fen: games[games.length - 1].game.fen(),
           }),
         );
         writeNewGame();
+        games[games.length - 1].connect(ws._socket.remoteAddress);
       }
     }
   });
@@ -123,10 +148,12 @@ const writeNewGame = () => {
   // fs.writeFile('./games.json', JSON.stringify(games), (e) => {
   //   if (e) throw e;
   // });
-  console.log('writing new game');
+  console.log('writing new game', games[games.length - 1].code);
 };
 
 //start our server
 server.listen(process.env.PORT || 8999, () => {
   console.log(`Server started on port ${server.address().port} :)`);
 });
+// })
+// .catch(console.error);
